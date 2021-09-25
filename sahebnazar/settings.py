fc3fdd636a650d6781a4995252b363584d657f67
momenian <hamomenian@gmail.com>
2021-09-26 00:43:11 +0330

    add app

--- sahebnazar/settings.py
+++ sahebnazar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-#iu%ucugj09hbwz%=wl4o#nweckixbc(n)kp%dn&)zdx)t)r-u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
