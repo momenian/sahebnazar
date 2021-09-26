005e729680ce8890ab8d9b970a5d1c358afbc60a
momenian <hamomenian@gmail.com>
2021-09-27 00:59:21 +0330

    fixed secret leak issue

--- sahebnazar/settings.py
+++ sahebnazar/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'learning_material',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'sahebnazar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sahebnazar',
+        'USER': 'momenian',
+        'PASSWORD': 'salamsalam',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
